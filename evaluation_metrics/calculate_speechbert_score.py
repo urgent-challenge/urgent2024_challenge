ecc70c7c11c560d9d80ae736a41245753ad13120
Wangyou Zhang <C0me_On@163.com>
2024-02-15 10:37:06 -0500

    Add SpeechBERTScore metric and a script to obtain breakdown results

---
+++ evaluation_metrics/calculate_speechbert_score.py
@@ -0,0 +1,175 @@
+from pathlib import Path
+
+import numpy as np
+import soundfile as sf
+import soxr
+import torch
+from tqdm import tqdm
+
+from discrete_speech_metrics import SpeechBERTScore as SBS
+
+
+METRICS = ("SpeechBERTScore",)
+TARGET_FS = 16000
+
+
+################################################################
+# Definition of metrics
+################################################################
+class SpeechBERTScore:
+    """SpeechBERTScore.
+
+    Reference:
+        SpeechBERTScore: Reference-Aware Automatic Evaluation of Speech
+        Generation Leveraging NLP Evaluation Metrics
+        https://arxiv.org/abs/2401.16812
+    """
+
+    def __init__(self, device="cpu"):
+        self.speech_bert_score = SBS(
+            sr=TARGET_FS, model_type="wavlm-large", layer=14, use_gpu="cuda" in device
+        )
+
+    def __call__(self, reference: np.ndarray, sample: np.ndarray) -> float:
+        precision, recall, f1_score = self.speech_bert_score.score(reference, sample)
+        return precision, recall, f1_score
+
+
+def speech_bert_score_metric(model, ref, inf, fs=16000):
+    """Calculate the SpeechBERTScore between ref and inf.
+
+    Args:
+        model (torch.nn.Module): SpeechBERTScore model
+            Please use the model with model_type="wavlm-large" and layer=14
+            to get comparable results.
+        ref (np.ndarray): reference signal (time,)
+        inf (np.ndarray): enhanced signal (time,)
+        fs (int): sampling rate in Hz
+    Returns:
+        score (float): SpeechBERTScore precision value between [0, 1]
+    """
+    if fs != TARGET_FS:
+        ref = soxr.resample(ref, fs, TARGET_FS)
+        inf = soxr.resample(inf, fs, TARGET_FS)
+    with torch.no_grad():
+        score = model(ref, inf)
+    return score[0]
+
+
+################################################################
+# Main entry
+################################################################
+def main(args):
+    refs = {}
+    with open(args.ref_scp, "r") as f:
+        for line in f:
+            uid, audio_path = line.strip().split()
+            refs[uid] = audio_path
+
+    data_pairs = []
+    with open(args.inf_scp, "r") as f:
+        for line in f:
+            uid, audio_path = line.strip().split()
+            data_pairs.append((uid, refs[uid], audio_path))
+
+    size = len(data_pairs)
+    assert 1 <= args.job <= args.nsplits <= size
+    interval = size // args.nsplits
+    start = (args.job - 1) * interval
+    end = size if args.job == args.nsplits else start + interval
+    data_pairs = data_pairs[start:end]
+    print(
+        f"[Job {args.job}/{args.nsplits}] Processing ({len(data_pairs)}/{size}) samples",
+        flush=True,
+    )
+    suffix = "" if args.nsplits == args.job == 1 else f".{args.job}"
+
+    outdir = Path(args.output_dir)
+    outdir.mkdir(parents=True, exist_ok=True)
+    writers = {
+        metric: (outdir / f"{metric}{suffix}.scp").open("w") for metric in METRICS
+    }
+
+    model = SpeechBERTScore(device=args.device)
+    model.speech_bert_score.model.eval()
+    ret = []
+    for uid, ref_audio, inf_audio in tqdm(data_pairs):
+        _, score = process_one_pair((uid, ref_audio, inf_audio), model=model)
+        ret.append((uid, score))
+        for metric, value in score.items():
+            writers[metric].write(f"{uid} {value}\n")
+
+    for metric in METRICS:
+        writers[metric].close()
+
+    if args.nsplits == args.job == 1:
+        with (outdir / "RESULTS.txt").open("w") as f:
+            for metric in METRICS:
+                mean_score = np.nanmean([score[metric] for uid, score in ret])
+                f.write(f"{metric}: {mean_score:.4f}\n")
+        print(
+            f"Overall results have been written in {outdir / 'RESULTS.txt'}", flush=True
+        )
+
+
+def process_one_pair(data_pair, model=None):
+    uid, ref_path, inf_path = data_pair
+    ref, fs = sf.read(ref_path, dtype="float32")
+    inf, fs2 = sf.read(inf_path, dtype="float32")
+    assert fs == fs2, (fs, fs2)
+    assert ref.shape == inf.shape, (ref.shape, inf.shape)
+    assert ref.ndim == 1, ref.shape
+
+    scores = {}
+    for metric in METRICS:
+        if metric == "SpeechBERTScore":
+            scores[metric] = speech_bert_score_metric(model, ref, inf, fs=fs)
+        else:
+            raise NotImplementedError(metric)
+
+    return uid, scores
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ref_scp",
+        type=str,
+        required=True,
+        help="Path to the scp file containing reference signals",
+    )
+    parser.add_argument(
+        "--inf_scp",
+        type=str,
+        required=True,
+        help="Path to the scp file containing enhanced signals",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="Path to the output directory for writing metrics",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cpu",
+        help="Device for running speaker embedding extraction",
+    )
+    parser.add_argument(
+        "--nsplits",
+        type=int,
+        default=1,
+        help="Total number of computing nodes to speed up evaluation",
+    )
+    parser.add_argument(
+        "--job",
+        type=int,
+        default=1,
+        help="Index of the current node (starting from 1)",
+    )
+    args = parser.parse_args()
+
+    main(args)
---
+++ evaluation_metrics/get_breakdown.py
@@ -0,0 +1,154 @@
+from collections import defaultdict
+import json
+
+import numpy as np
+
+
+#############################
+# Group function definitions
+#############################
+def group_by_fs(meta):
+    """Return a unique group id based on the fs in the given meta dictionary."""
+    # group by sampling rate
+    fs = int(meta["fs"])
+    return f"fs={fs}Hz"
+
+
+def group_by_snr(meta):
+    """Return a unique group id based on SNR in the given meta dictionary."""
+    # group every 5 dB
+    snr = int(float(meta["snr_dB"]) / 5) * 5
+    return f"snr={snr:02d}dB"
+
+
+def group_by_duration(meta):
+    """Return a unique group id based on the duration in the given meta dictionary."""
+    # group by sample length (every 5s)
+    length = int((float(meta["length"]) / float(meta["fs"])) / 5) * 5
+    return f"duration={length:02d}s"
+
+
+def group_by_corpus(meta):
+    """Return a unique group id based on the corpus in the given meta dictionary."""
+    # group by corpus name (prefix of speech_sid)
+    corpus = meta["speech_sid"].split("_", maxsplit=1)[0]
+    return f"corpus={corpus}"
+
+
+def group_by_rir(meta):
+    """Return a unique group id based on the RIR in the given meta dictionary."""
+    # group by whether RIR is applied or not
+    rir = meta["rir_uid"] != "none"
+    return "with_rir" if rir else "no_rir"
+
+
+def group_by_augmentation(meta):
+    """Return a unique group id based on augmentation in the given meta dictionary."""
+    # group by augmentation type
+    augmentation = meta["augmentation"]
+    if augmentation.startswith("bandwidth_limitation"):
+        return "bandwidth_limitation"
+    return augmentation
+
+
+def group_score_by_field(result_dic, meta_dic, group_func):
+    """Group scores of different samples in `result_dic`
+    by the value in the given field in `meta_dic`.
+
+    Args:
+        result_dic (dict): A dictionary containing scores of different samples.
+        meta_dic (dict): A dictionary containing meta information of different samples.
+        group_func (callable): A function defining how to group the samples.
+    Returns:
+        ret (dict): A dictionary of groups of scores.
+    """
+    ret = defaultdict(list)
+    for uid, score in result_dic.items():
+        group = group_func(meta_dic[uid])
+        ret[group].append(score)
+    return dict(ret)
+
+
+#############################
+# Main entry
+#############################
+def main(args):
+    result_dic = {}
+    is_wer = None
+    with open(args.result_scp, "r") as f:
+        for line in f:
+            uid, score = line.strip().split(maxsplit=1)
+            if is_wer is None:
+                try:
+                    float(score)
+                    is_wer = False
+                except ValueError:
+                    is_wer = True
+            score = json.loads(score) if is_wer else float(score)
+            if not uid.startswith("fileid"):
+                uid = "fileid" + uid.split("fileid", maxsplit=1)[1]
+            result_dic[uid] = score
+
+    meta_dic = {}
+    with open(args.meta_tsv, "r") as f:
+        headers = next(f).strip().split("\t")
+        for line in f:
+            tup = line.strip().split("\t")
+            meta_dic[tup[0]] = dict(zip(headers, tup))
+
+    for group_func in (
+        group_by_fs,
+        group_by_snr,
+        group_by_duration,
+        group_by_corpus,
+        group_by_rir,
+        group_by_augmentation,
+    ):
+        group_func_name = group_func.__name__.replace("group_by_", "")
+        groups = group_score_by_field(result_dic, meta_dic, group_func)
+        if group_func in (group_by_fs, group_by_snr, group_by_duration):
+            groups = {k: groups[k] for k in sorted(groups.keys())}
+        print(f"\n====== Group by {group_func_name} =====\n")
+        for group, dic in groups.items():
+            msg = f"[Group] {group}\n\t"
+            msg += get_average_score(dic, is_wer=is_wer)
+            print(msg)
+
+
+def get_average_score(result_lst, is_wer=False):
+    """Return the average score of all samples in the given result list."""
+    if is_wer:
+        dic = {"delete": 0, "insert": 0, "replace": 0, "equal": 0}
+        for score in result_lst:
+            for k in dic.keys():
+                dic[k] = dic[k] + score[k]
+        numerator = dic["replace"] + dic["delete"] + dic["insert"]
+        denominator = dic["replace"] + dic["delete"] + dic["equal"]
+        wer = numerator / denominator
+        msg = f"WER: {wer:.4f}\n"
+        for op, count in dic.items():
+            msg += f"    {op}: {count}\n"
+    else:
+        score = np.nanmean(result_lst)
+        msg = f"Average score: {score}\n"
+    return msg
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "result_scp",
+        type=str,
+        help="Path to the scp file containing evaluation result of each sample.",
+    )
+    parser.add_argument(
+        "--meta_tsv",
+        type=str,
+        required=True,
+        help="Path to the tsv file containing meta information about each sample",
+    )
+    args = parser.parse_args()
+
+    main(args)
