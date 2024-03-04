Add scale normalization and fix LSD calculation

evaluation_metrics/calculate_intrusive_se_metrics.py
@@ -46,7 +46,7 @@ def get_2fmodel_metric(ref, inf, fs=48000):
     Returns:
         ret (float): 2f-model value between [0, 100]
     """
-    return
+    raise NotImplementedError
 
 
 def estoi_metric(ref, inf, fs=16000):
@@ -75,29 +75,33 @@ def lsd_metric(ref, inf, fs, nfft=0.032, hop=0.016, p=2, eps=1.0e-08):
         eps (float): epsilon value for numerical stability
     Returns:
         mcd (float): LSD value between [0, +inf)
-            when p=2, the unit is dB.
     """
+    scaling_factor = np.sum(ref * inf) / (np.sum(inf**2) + eps)
+    inf = inf * scaling_factor
+
     nfft = int(fs * nfft)
     hop = int(fs * hop)
     # T x F
     ref_spec = np.abs(librosa.stft(ref, hop_length=hop, n_fft=nfft)).T
     inf_spec = np.abs(librosa.stft(inf, hop_length=hop, n_fft=nfft)).T
-    lsd = np.log10(ref_spec**2 / ((inf_spec + eps) ** 2) + eps) ** p
+    lsd = np.log(ref_spec**2 / ((inf_spec + eps) ** 2) + eps) ** p
     lsd = np.mean(np.mean(lsd, axis=1) ** (1 / p), axis=0)
     return lsd
 
 
-def mcd_metric(ref, inf, fs):
+def mcd_metric(ref, inf, fs, eps=1.0e-08):
     """Calculate Mel Cepstral Distortion (MCD).
 
     Args:
         ref (np.ndarray): reference signal (time,)
         inf (np.ndarray): enhanced signal (time,)
         fs (int): sampling rate in Hz
+        eps (float): epsilon value for numerical stability
     Returns:
         mcd (float): MCD value between [0, +inf)
     """
-    return calculate_mcd(ref, inf, fs)
+    scaling_factor = np.sum(ref * inf) / (np.sum(inf**2) + eps)
+    return calculate_mcd(ref, inf * scaling_factor, fs)
 
 
 def pesq_metric(ref, inf, fs=8000):